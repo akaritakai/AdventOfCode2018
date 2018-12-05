fb5f824a2fa70136d5a3a07b252215cdddfd595e
Justin Kaufman <akaritakai@gmail.com>
2018-12-04 23:43:50 -0800

    Speed up Problem 3

--- src/net/akaritakai/aoc2018/Problem3.java
+++ src/net/akaritakai/aoc2018/Problem3.java
@@ -28,20 +28,80 @@ public class Problem3 extends AbstractProblem {
 
     @Override
     public String solvePart2() {
-        return getClaims().parallelStream()
+        // Assume any claim could be a candidate
+        final var candidateClaims = getClaims();
+
+        // Speed-up: Test for overlaps within quadrants and remove any matches from our list of candidates
+        final var overlappingClaims = subDivideClaimArea(candidateClaims)
+                .parallelStream()
+                .flatMap(region -> getOverlappingClaims(candidateClaims, region).stream())
+                .collect(Collectors.toSet());
+        candidateClaims.removeAll(overlappingClaims);
+
+        // Find the claim that doesn't overlap any other
+        final var claim = candidateClaims.parallelStream()
                 .filter(claim1 -> getClaims().parallelStream()
                         .filter(claim2 -> claim1.id != claim2.id)
                         .noneMatch(claim2 -> claim1.rectangle.intersects(claim2.rectangle)))
-                .findAny()
-                .map(claim -> String.valueOf(claim.id))
-                .orElse(null);
+                .findAny();
+
+        // Return the claim's id
+        return claim.map(c -> c.id).map(String::valueOf).orElse(null);
     }
 
-    private List<Claim> getClaims() {
+    /**
+     * Divides the claim area into quadrants based on the claims provided
+     */
+    private List<Rectangle> subDivideClaimArea(final Set<Claim> claims) {
+        final var maxWidth = claims.stream()
+                .map(claim -> claim.rectangle.x + claim.rectangle.width)
+                .max(Integer::compare)
+                .orElse(-1);
+
+        final var maxHeight = claims.stream()
+                .map(claim -> claim.rectangle.y + claim.rectangle.height)
+                .max(Integer::compare)
+                .orElse(-1);
+
+        return divideIntoQuadrants(new Rectangle(0, 0, maxWidth, maxHeight));
+    }
+
+    /**
+     * Divides a given rectangle into its quadrants
+     */
+    private List<Rectangle> divideIntoQuadrants(final Rectangle region) {
+        return List.of(new Rectangle(0, 0, region.width / 2, region.height / 2),
+                new Rectangle(0, region.height / 2, region.width / 2, region.height / 2),
+                new Rectangle(region.width / 2, 0, region.width / 2, region.height / 2),
+                new Rectangle(region.width / 2, region.height / 2, region.width / 2, region.height / 2));
+    }
+
+    /**
+     * Gets the claims in the region bound by the given rectangle and find the non-overlapping claims
+     */
+    private Set<Claim> getOverlappingClaims(final Set<Claim> claims, final Rectangle region) {
+        final var subClaims = claims.parallelStream()
+                .filter(claim -> region.contains(claim.rectangle))
+                .collect(Collectors.toSet());
+        return getOverlappingClaims(subClaims);
+    }
+
+    /**
+     * Gets the set of claims that overlap another in a given set
+     */
+    private Set<Claim> getOverlappingClaims(final Set<Claim> claims) {
+         return claims.parallelStream()
+                .filter(claim1 -> claims.parallelStream()
+                        .filter(claim2 -> claim1.id != claim2.id)
+                        .anyMatch(claim2 -> claim1.rectangle.intersects(claim2.rectangle)))
+                .collect(Collectors.toSet());
+    }
+
+    private Set<Claim> getClaims() {
         return getPuzzleInputLines()
                 .stream()
                 .map(Claim::fromString)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
     }
 
     static class Claim {
@@ -73,5 +133,17 @@ public class Problem3 extends AbstractProblem {
             return new Claim(id, new Rectangle(x, y, width, height));
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Claim claim = (Claim) o;
+            return id == claim.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return id;
+        }
     }
 }
